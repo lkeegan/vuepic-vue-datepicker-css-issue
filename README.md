b68bb2786f54bf609da3189aeb0864878b69259e
lkeegan <liam@keegan.ch>
2023-06-23 14:19:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Playground for @vuepic/vue-datepicker
+
+- Github: https://github.com/Vuepic/vue-datepicker
+- Documentation: https://vue3datepicker.com
---
+++ src/components/Playground.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Datepicker v-model="date" :enable-time-picker="false" :start-date="new Date(1980, 1, 1)" :prevent-min-max-navigation="true" :hide-navigation="['month', 'year', 'calender', 'time']" no-today inline :auto-apply="true" :keep-action-row="false" :flow="['year', 'month', 'calendar']" style="width: 400pt; padding-left: 10pt"/>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
+
+const date = ref();
+</script>
